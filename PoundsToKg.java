e28511cdb3247eba576aff354030cc18a7c799ab
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-12-05 17:22:47 +0400

    Add files via upload

---
+++ PoundsToKg.java
@@ -0,0 +1,8 @@
+public class PoundsToKg {
+    public static void main(String[] args) {
+
+        double pounds = 1000d;
+                double poundsToKg = pounds * 0.45359237d;
+        System.out.println(poundsToKg);
+    }
+}
