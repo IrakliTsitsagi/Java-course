a287c3ee9f0168b9974827c4ec1cddd9da07663a
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2022-12-23 14:34:39 +0400

    Update Foot2Inch.java

--- Foot2Inch.java
+++ Foot2Inch.java
@@ -1,20 +1,21 @@
 public class Foot2Inch {
     public static void main(String[] args) {
-        System.out.println(calcFeetAndInchesToCantimeters(2, 11));
-calcFeetAndInchesToCantimeters(157);
+        calcFeetAndInchesToCantimeters(1, 11);
+        calcFeetAndInchesToCantimeters(157);
     }
 
     public static double calcFeetAndInchesToCantimeters(double feet, double inches) {
-        if (feet < 0) {
-        } else if (inches < 0) {
-            return -1;
-        } else if (inches > 12) {
+        if ((feet < 0) || (inches < 0) || (inches > 12)) {
+            System.out.println("invalid parameters");
             return -1;
         }
+
+
         double centimeters = (feet * 12) * 2.54;
         centimeters += inches * 2.54;
         System.out.println(feet + " feet, " + inches + " inches = " + centimeters + "cm");
         return centimeters;
+
     }
 
     public static double calcFeetAndInchesToCantimeters(double inches) {
@@ -23,11 +24,9 @@ calcFeetAndInchesToCantimeters(157);
         }
         double feet = (int) inches / 12;
         double remainingInches = (int) inches % 12;
-        System.out.println(inches + " inches is equal to "
-                + feet + " feet and " + remainingInches + " inches");
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
         return calcFeetAndInchesToCantimeters(feet, remainingInches);
     }
 
+
 }
-
-
